fec7f131257b7a40571a9a1d0969e4ab24c21b81
Alix Fullerton <fullertonalix0@gmail.com>
2018-06-20 04:35:35 -0600

    What appears to be relatively working markdown render

--- app/utilities/draw.py
+++ app/utilities/draw.py
@@ -5,6 +5,8 @@ Draws all the different types of responses for Simple Honey
 import os
 
 from flask import send_file, redirect, render_template, Response
+from jinja2 import Markup
+import markdown2
 
 import app
 
@@ -36,18 +38,17 @@ def file(path):
 
 def template(template_file, requested_uri):
     """
-    Draws any template within app/templates. This method should probably be moved to a utility.
+    Draws any template within app/templates.
 
     :param template_file: Template file path relative to app/templates.
     :type template_file: str
     :param requested_uri: Uri the client hit on.
     :type requested_uri: str
-    :returns: Rendered jinja template.
+    :returns: Rendered jinja template, ready for sending to client.
     :rtype: str
     """
-    data = {}
-    data['options'] = app.global_content['options']
-    data['requested'] = requested_uri
+    data = _base_template_arguments(requested_uri)
+
     return render_template(template_file, **data)
 
 
@@ -71,6 +72,7 @@ def nothing():
     """
     data = {}
     data['options'] = app.global_content['options']
+
     return render_template('boiler.html', **data)
 
 
@@ -96,11 +98,56 @@ def redirect_client(requested_uri):
     """
     return redirect(requested_uri['value'], 301)
 
+
 def custom_template(requested_uri):
+    """
+
+    :param requested_uri: The Uri info to be redirected.
+    :type requested_uri: dict
+    """
     tempalte_path = os.path.join('/data/hosted_files/templates', requested_uri['value'])
     if not os.path.exists(tempalte_path):
         return nothing()
     phile = open(tempalte_path)
+
     return phile.read()
+
+
+def markdown(requested_uri):
+    """
+
+    :param requested_uri: The Uri info to be redirected.
+    :type requested_uri: dict
+    """
+    html = ''
+    title = ''
+    markdown_template = os.path.join('/data/hosted_files', requested_uri['value'])
+    if os.path.exists(markdown_template):
+        html = markdown2.markdown_path(markdown_template)
+    else:
+        html = markdown2.markdown(requested_uri['value'])
+
+    if requested_uri['value']:
+        title = requested_uri['value']
+
+    data = _base_template_arguments(requested_uri)
+    data['title'] = title
+    data['markdown'] = Markup(html)
+    return render_template('uris/markdown.html', **data)
+
+
+def _base_template_arguments(requested_uri):
+    """
+    The minimal dynamic arguments that we will send to almost every template.
+
+    :param requested_uri: The Uri info to be redirected.
+    :type requested_uri: dict
+    :returns: Global options, and the clients Request information.markdown
+    :rtype: dict
+    """
+    data = {}
+    data['options'] = app.global_content['options']
+    data['requested'] = requested_uri
+    return data
 
 # End File: simple-honey/app/utilities/draw.py
